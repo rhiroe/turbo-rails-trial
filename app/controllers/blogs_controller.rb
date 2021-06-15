0874de323227c6ac62c6da5e84e95bf891fe952e
rhiroe <ride.poke@gmail.com>
2021-06-15 14:50:23 +0900

    簡単な検索処理を追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,10 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
-    @blogs = Blog.all
+    respond_to do |format|
+      format.html { @blogs = Blog.all }
+      format.js { @blogs = Blog.search_by_title(params[:search_title]) }
+    end
   end
 
   # GET /blogs/1 or /blogs/1.json
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,3 @@
 class Blog < ApplicationRecord
+  scope :search_by_title, ->(title) { where('title like ?', "%#{title}%") }
 end
